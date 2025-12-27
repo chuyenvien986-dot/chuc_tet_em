e0b74fccd89bfd66b083db2632efd817b15c7202
chuyenvien986-dot <chuyenvien986@gmail.com>
2025-12-27 08:10:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 
+const PASSWORD = "25/5"; // 🔐 đổi mật khẩu tại đây
+
+const openBtn = document.getElementById("openBtn");
+const passwordInput = document.getElementById("password");
+const errorText = document.getElementById("error");
+
+const lock = document.getElementById("lock");
+const card = document.getElementById("card");
+const music = document.getElementById("music");
+
+openBtn.addEventListener("click", () => {
+  if(passwordInput.value === PASSWORD){
+    lock.style.display = "none";
+    card.style.display = "block";
+
+    music.volume = 0.7;
+    music.play().catch(() => {
+      alert("Không phát được nhạc. Kiểm tra file music.mp3");
+    });
+  }else{
+    errorText.innerText = "Sai mật khẩu";
+  }
+});
